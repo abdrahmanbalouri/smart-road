7abb0d028d89c043b1e3713c87c5d1e4653491f4
abdrahmanbalouri <balouriabdrahman@gmail.com>
2026-02-03 05:56:35 +0100

    drawing car

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,10 @@
 */
 
 use rand::Rng;
- use sdl2::event::Event;
-// #[cfg(feature = "sdl_image")]
-// use sdl2::image::{self, LoadTexture};
-// #[cfg(feature = "sdl_ttf")]
-// use sdl2::ttf;
- use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::pixels::PixelFormatEnum;
+//  use sdl2::keyboard::Keycode;
+// use sdl2::pixels::Color;
 // use sdl2::pixels::PixelFormatEnum;
 // use sdl2::rect::Rect;
 // use std::collections::VecDeque;
@@ -29,7 +26,7 @@ use std::time::Duration;
 
 mod help;
 //use help::*;
-pub fn main() {
+pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
@@ -40,29 +37,44 @@ pub fn main() {
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
+    let texture_creator = canvas.texture_creator();
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    // Load image with the `image` crate and upload into a streaming texture
+    let img = image::open("src/img/car.png").unwrap().into_rgba8();
+    let (w, h) = img.dimensions();
+
+    let mut texture = texture_creator
+        .create_texture_streaming(PixelFormatEnum::RGBA8888, w, h)
+        .unwrap();
+
+    let pixels = img.into_raw();
+
+    texture
+        .with_lock(None, |buf, pitch| {
+            let row = (w as usize) * 4;
+            for y in 0..h as usize {
+                let src = y * row;
+                let dst = y * pitch;
+                buf[dst..dst + row].copy_from_slice(&pixels[src..src + row]);
+            }
+        })
+        .unwrap();
+
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
+                Event::Quit { .. } => break 'running,
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
-
+        canvas.clear();
+        // draw car texture in top-left (example)
+          canvas.copy(&texture, None, None).ok();
+        // Drawing code would go here
         canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        ::std::thread::sleep(Duration::new(0, 1_000_00000u32 / 60));
     }
+
+    Ok(())
 }
